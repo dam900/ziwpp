5d57c25ae8bfb0af68dbf34e0ce0d3db883982e0
dam900 <damianjakubowski48@gmail.com>
2025-12-21 22:03:33 +0100

    added simple socket and client

--- main.cpp
+++ main.cpp
@@ -1,18 +1,48 @@
-#include "src/solver.h"
+#include <cstring>
+#include <iostream>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+void handle_client(int client) {
+  char buffer[1024] = {0};
+  int bytes_received = recv(client, buffer, sizeof(buffer) - 1, 0);
+
+  if (bytes_received > 0) {
+    buffer[bytes_received] = '\0';
+    std::cout << "Received: " << buffer << std::endl;
+    send(client, buffer, bytes_received, 0);
+  }
+
+  close(client);
+}
 
 int main() {
-    std::string basePath = ".././data/scheduling-benchmarks/wtsds/";  
-    std::string instanceName = "wt_sds_2.instance";  
 
-    std::string instancePath = basePath + instanceName;
+  int sock = socket(AF_INET, SOCK_STREAM, 0);
+  sockaddr_in addr;
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(8080);
+  addr.sin_addr.s_addr = INADDR_ANY;
 
-    solver::ProblemInstance instance = solver::readInstance(instancePath);
+  auto fd = bind(sock, (sockaddr *)&addr, sizeof(addr));
+  listen(sock, 1);
 
-    solver::Solution bestSolution = solver::simulatedAnnealing(instance, 100000, 1000.0, 0.995);
-    std::cout << "Best TWT found: " << bestSolution.objectiveValue << std::endl;
+  while (true) {
+    int client = accept(sock, nullptr, nullptr);
+    handle_client(client);
+  }
 
-    solver::Solution insertionSolution = solver::TWT_Insertion_Heuristic(instance);
-    std::cout << "Insertion Heuristic TWT: " << insertionSolution.objectiveValue << std::endl;
+  close(sock);
+  // solver::ProblemInstance instance = solver::readInstance(instancePath);
 
-    return 0;
+  // solver::Solution bestSolution = solver::simulatedAnnealing(instance,
+  // 100000, 1000.0, 0.995); std::cout << "Best TWT found: " <<
+  // bestSolution.objectiveValue << std::endl;
+
+  // solver::Solution insertionSolution =
+  // solver::TWT_Insertion_Heuristic(instance); std::cout << "Insertion
+  // Heuristic TWT: " << insertionSolution.objectiveValue << std::endl;
+
+  return 0;
 }
